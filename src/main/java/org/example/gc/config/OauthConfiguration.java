6ded68db18599e1595fb5c328bb5bccc282213a4
kkk12345678 <kkk12345678@users.noreply.github.com>
2023-06-27 15:15:03 +0300

    Oauth2 added

---
+++ src/main/java/org/example/gc/config/OauthConfiguration.java
@@ -0,0 +1,56 @@
+package org.example.gc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class OauthConfiguration {
+    private static final List<String> clients = Arrays.asList("google", "facebook");
+    private static final String CLIENT_PROPERTY_KEY
+            = "spring.security.oauth2.client.registration.";
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        List<ClientRegistration> registrations = clients.stream()
+                .map(this::getRegistration)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+        return new InMemoryClientRegistrationRepository(registrations);
+    }
+
+    private ClientRegistration getRegistration(String client) {
+        String clientId = env.getProperty(
+                CLIENT_PROPERTY_KEY + client + ".client-id");
+
+        if (clientId == null) {
+            return null;
+        }
+
+        String clientSecret = env.getProperty(
+                CLIENT_PROPERTY_KEY + client + ".client-secret");
+
+        if (client.equals("google")) {
+            return CommonOAuth2Provider.GOOGLE.getBuilder(client)
+                    .clientId(clientId).clientSecret(clientSecret).build();
+        }
+        if (client.equals("facebook")) {
+            return CommonOAuth2Provider.FACEBOOK.getBuilder(client)
+                    .clientId(clientId).clientSecret(clientSecret).build();
+        }
+        return null;
+    }
+}
--- src/main/java/org/example/gc/config/SecurityConfiguration.java
+++ src/main/java/org/example/gc/config/SecurityConfiguration.java
@@ -8,16 +8,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 
 @Configuration
@@ -25,12 +39,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @ComponentScan(basePackages = {"org.example.gc"})
 @Slf4j
 public class SecurityConfiguration {
-    private static final String GUEST_UPDATE = "/users";
+    private static final String GUEST_UPDATE = "/users/*";
     private static final String[] USER_UPDATE = {"/users/*", "/orders/*", "/orders"};
     private static final String[] ADMIN_UPDATE =
             {"/tags/*", "/gift-certificates/*", "/orders/*", "/users/*", "/tags", "/gift-certificates", "/orders", "/users"};
     private static final String[] GUEST_READ =
-            {"/tags/*", "/gift-certificates/*", "/tags/*", "/gift-certificates/*"};
+            {"/tags", "/gift-certificates", "/tags/*", "/gift-certificates/*"};
     private static final String[] USER_READ =
             {"/tags/*", "/gift-certificates/*", "/tags/*", "/gift-certificates/*", "/users/*"};
     @Autowired
@@ -49,6 +63,7 @@ public class SecurityConfiguration {
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public DaoAuthenticationProvider authProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -57,6 +72,7 @@ public class SecurityConfiguration {
         return authProvider;
     }
 
+/*
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -70,10 +86,22 @@ public class SecurityConfiguration {
                             }
                         })))
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/users/login", "users/signup").permitAll()
-                        //.requestMatchers(HttpMethod.GET, ADMIN_UPDATE).hasAuthority("admin")
+                        .requestMatchers(HttpMethod.GET, GUEST_READ).permitAll()
+                        .requestMatchers(GUEST_UPDATE).permitAll()
+                        .requestMatchers(HttpMethod.GET, ADMIN_UPDATE).hasAuthority("admin")
                         .anyRequest().authenticated())
                 .apply(new JwtConfigurer(jwtTokenProvider));
+        return http.build();
+    }
+
+
+ */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(r -> r.anyRequest().authenticated())
+                .oauth2ResourceServer(c -> c.jwt(Customizer.withDefaults()))
+                .oauth2Login(Customizer.withDefaults());
         return http.build();
     }
 }
